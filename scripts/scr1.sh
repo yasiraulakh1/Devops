5834f00fffc2e00b0bf55ebd6008fffac80062cc
yasir <yasir@localhost.localdomain>
2023-09-06 18:36:56 +0500

    new files added

---
+++ scripts/scr1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+set -x # enable debug mode (now you can see the commands which execute)
+set -e # when any error in scrpt then stop the script at that time and exit
+set -o pipefail 
+
+echo "####################################"
+echo " this is the test script"
+echo "#####################################"
+
+
+
+
+
+
